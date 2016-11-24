e275f14cc750cada1e18a3b0b2f42d8ec455523d
Savitha <savithasam@live.com>
2016-11-24 23:41:04 +0530

    first commit

---
+++ image_ss.py
@@ -0,0 +1,27 @@
+import selectivesearch
+import cv2
+
+def main():
+    
+    img =  cv2.imread('1.jpg')
+    # perform selective search
+    img_lbl, regions = selectivesearch.selective_search(
+        img, scale=500, sigma=0.9, min_size=10)
+
+    candidates = set()
+    for r in regions:
+        # excluding same rectangle (with different segments)
+        if r['rect'] in candidates:
+            continue
+        # excluding regions smaller than 2000 pixels
+        if r['size'] < 2000:
+            continue
+        # distorted rects
+        x, y, w, h = r['rect']
+        if w / h > 1.2 or h / w > 1.2:
+            continue
+        candidates.add(r['rect'])
+    print candidates
+      
+if __name__ == "__main__":
+    main()
